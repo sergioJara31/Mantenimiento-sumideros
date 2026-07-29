632e286f1b532a11195ce99dea634847c42e9cf3
sergiojara31 <sergio.jaramillob@autonoma.edu.co>
2026-07-29 12:04:15 -0500

    first commit

---
+++ kobo_api.py
@@ -0,0 +1,616 @@
+import os
+import pandas as pd
+import requests
+import sys
+import unicodedata
+from openpyxl.drawing.image import Image as OpenpyxlImage
+from PIL import Image as PILImage
+from dotenv import load_dotenv
+from openpyxl import load_workbook
+from datetime import datetime
+from openpyxl.styles import Font, PatternFill, Alignment, Border, Side
+from openpyxl.utils import get_column_letter
+from openpyxl.drawing.image import Image
+from collections import defaultdict
+from openpyxl.drawing.spreadsheet_drawing import (
+    AnchorMarker,
+    OneCellAnchor
+)
+from openpyxl.utils.units import pixels_to_EMU
+from openpyxl.drawing.xdr import XDRPositiveSize2D
+load_dotenv()
+
+
+import os
+import requests
+
+def crear_sesion():
+    token = os.getenv("APIKEY")
+
+    if not token:
+        raise ValueError("No se encontró la variable de entorno APIKEY.")
+
+    session = requests.Session()
+    session.headers.update({
+        "Authorization": f"Token {token}"
+    })
+
+    return session
+
+""" obtener el uid del formualriod e donde se obtendra la información de los sumideros """
+def obtener_uid(session):
+    url = "https://kf.kobotoolbox.org/api/v2/assets/"
+
+    response = session.get(url)
+    response.raise_for_status()
+
+    datos = response.json()
+
+    return datos["results"][0]["uid"]
+
+
+def obtener_formulario(session, uid):
+    url = f"https://kf.kobotoolbox.org/api/v2/assets/{uid}/data/"
+
+    response = session.get(url)
+    response.raise_for_status()
+
+    return response.json()
+    
+
+def procesar_datos(formulario):
+    sumideros=[]
+    sumideros_sin_id = []
+    
+    for sumidero in formulario["results"]:
+        foto_antes = ""
+        foto_despues = ""
+        # Buscar las URLs de las fotos del antes y despues
+        for adj in sumidero.get("_attachments", []):
+            if adj.get("question_xpath") == "TOME_UNA_FOTO_DEL_SU_ES_DEL_MANTENIMIENTO":
+                foto_antes = adj.get("download_url", "")
+
+            elif adj.get("question_xpath") == "TOME_UNA_FOTO_DEL_SU_ES_DEL_MANTENIMIENTO_001":
+                foto_despues = adj.get("download_url", "")  
+        registro = {
+            "ID_ELEMENTO": (
+                f"{(sumidero.get('ID') if sumidero.get('ID') is not None else sumidero.get('ID_sumidero'))}s"
+                if (sumidero.get('ID') if sumidero.get('ID') is not None else sumidero.get('ID_sumidero')) is not None
+                else ""
+            ),
+            "SUBTIPO": sumidero.get("SUBTIPO").upper() if sumidero.get("SUBTIPO") else "",
+            "BARRIO": normalizar_barrio(sumidero.get("BARRIO")),            
+            "DIRECCIÓN": sumidero.get("DIRECCI_N").upper() if sumidero.get("DIRECCI_N") else "",
+            "ESTADO": sumidero.get("ESTADO").upper() if sumidero.get("ESTADO") else "",
+            "NUMERO REJAS": sumidero.get("NUMERO_DE_REJAS").upper() if sumidero.get("NUMERO_DE_REJAS") else "",
+            "PROFUNDIDAD": sumidero.get("PROFUNDIDAD"),
+            "MATERIAL TUBO": sumidero.get("MATERIAL").replace("_", " ").upper() if sumidero.get("MATERIAL") else "",
+            "DIAMETRO TUBO": sumidero.get("DIAMETRO_TUBO").upper() if sumidero.get("DIAMETRO_TUBO") else "",
+            "ENTREGA": sumidero.get("ENTREGA").upper() if sumidero.get("ENTREGA") else "",
+            "OBSERVACIONES": sumidero.get("OBSERVACIONES", "").upper() if sumidero.get("OBSERVACIONES") else "",
+            "FECHA LIMPIEZA": sumidero.get("today").upper() if sumidero.get("today") else "",
+            "OPERARIO": sumidero.get("OPERARIO").upper() if sumidero.get("OPERARIO") else "",
+            "URL_FOTOS_ANTES": foto_antes,
+            "URL_FOTOS_DESPUÉS": foto_despues
+        }
+        if sumidero.get("ID", ""):
+            sumideros.append(registro)
+        else:
+            sumideros_sin_id.append(registro)
+        
+    resultado= sumideros + sumideros_sin_id
+   
+    return resultado
+
+
+def normalizar_barrio(barrio):
+    if not barrio:
+        return ""
+
+    barrio = str(barrio).strip().upper()
+
+    return "".join(
+        c for c in unicodedata.normalize("NFD", barrio)
+        if unicodedata.category(c) != "Mn"
+    )
+
+def crear_excel(resultado_formulario, session):
+    df = pd.DataFrame(resultado_formulario)
+    archivo_excel = "Mantenimiento.xlsx"
+    columnas = [
+        "ID_ELEMENTO",
+        "SUBTIPO",
+        "BARRIO",
+        "DIRECCIÓN",
+        "ESTADO",
+        "NUMERO REJAS",
+        "PROFUNDIDAD",
+        "MATERIAL TUBO",
+        "DIAMETRO TUBO",
+        "ENTREGA",
+        "OBSERVACIONES",
+        "FECHA LIMPIEZA",
+        "OPERARIO",
+    ]
+
+    df = df[columnas]
+    
+
+    año = datetime.now().year
+    mes = datetime.now().month
+
+    periodo = 1 if mes <= 6 else 2
+
+    carpeta_semestre = f"Mantenimiento_Sumideros_{año}_{periodo}"
+
+    os.makedirs(carpeta_semestre, exist_ok=True)
+
+    carpeta_fotos = os.path.join(carpeta_semestre, "Fotos")
+    os.makedirs(carpeta_fotos, exist_ok=True)
+
+    archivo_excel = os.path.join(carpeta_semestre, "Mantenimiento.xlsx")
+    with pd.ExcelWriter(
+    archivo_excel,
+    engine="openpyxl") as writer:
+
+        for barrio, grupo in df.groupby("BARRIO"):
+        # Excel no permite nombres de hoja mayores a 31 caracteres
+            nombre_hoja = str(barrio)[:31]
+            grupo.to_excel(writer, sheet_name=nombre_hoja, index=False)
+
+    formatear_excel(archivo_excel)
+    descargar_fotos(resultado_formulario, session, carpeta_fotos)
+    insertar_imagenes(archivo_excel, resultado_formulario, carpeta_fotos)
+    
+
+    return df, archivo_excel, columnas
+
+
+def descargar_fotos(resultado_formulario, session, carpeta_fotos):
+    for registro in resultado_formulario:
+
+        barrio = normalizar_barrio(registro["BARRIO"])
+        id_elemento = registro.get("ID_ELEMENTO")
+
+        if  id_elemento == "" or id_elemento is None:
+            id_elemento = registro.get("DIRECCIÓN")
+
+        carpeta = os.path.join(carpeta_fotos, barrio)
+
+        os.makedirs(carpeta, exist_ok=True)
+
+        fotos = {
+            "antes": registro["URL_FOTOS_ANTES"],
+            "despues": registro["URL_FOTOS_DESPUÉS"]
+        }
+
+        for tipo, url in fotos.items():
+
+            if url == "":
+                continue
+
+            nombre = f"{id_elemento}_{tipo}.jpg"
+
+            ruta = os.path.join(carpeta, nombre)
+
+            if not os.path.exists(ruta):
+                try:
+                    r = session.get(url)
+                    if r.status_code == 200:
+                        with open(ruta, "wb") as f:
+                            f.write(r.content)
+                    else:
+                        print(f"Error al descargar {nombre}: código {r.status_code}")
+                except Exception as e:
+                    print(f"Error al procesar la URL {url}: {e}")
+   
+
+def formatear_excel(archivo_excel):
+
+    wb = load_workbook(archivo_excel)
+
+    for ws in wb.worksheets:
+
+        # Insertar espacio superior para encabezado
+        ws.insert_rows(1, amount=5)
+        ws.insert_cols(14, amount=2)
+        ws.cell(row=6, column=14).value = "Foto antes"
+        ws.cell(row=6, column=15).value = "Foto después"
+
+        # Título principal
+        ws.merge_cells("F2:J2")
+        ws["F2"] = "FORMATO LIMPIEZA DE SUMIDEROS - MANTENIMIENTO"
+        ws["F2"].font = Font(
+            bold=True,
+            size=12,
+            color="1F1F1F"
+        )
+        ws["F2"].alignment = Alignment(
+            horizontal="center"
+        )
+
+        # Contrato
+        ws.merge_cells("F3:J3")
+        ws["F3"] = "CONTRATO: 2026"
+        ws["F3"].font = Font(
+            bold=True
+        )
+
+        # Contratista
+        ws.merge_cells("F4:J4")
+        ws["F4"] = "CONTRATISTA: CONSTRUCTORA GAF SAS"
+        ws["F4"].font = Font(
+            bold=True
+        )
+        # Encabezado tabla
+        relleno = PatternFill(
+            "solid",
+            fgColor="C6E0B4"
+        )
+
+        fuente = Font(
+            bold=True,
+            color="006100"
+        )
+        for cell in ws[6]:
+            cell.fill = relleno
+            cell.font = fuente
+            cell.alignment = Alignment(
+                horizontal="center",
+                vertical="center",  
+            )
+
+        borde = Border(
+            left=Side(style="thin"),
+            right=Side(style="thin"),
+            top=Side(style="thin"),
+            bottom=Side(style="thin")
+        )
+
+        # Aplicar desde A6 hasta O(última fila)
+        for fila in ws.iter_rows(min_row=6, max_row=ws.max_row, min_col=1, max_col=15):
+
+            ws.row_dimensions[fila[0].row].height = 140
+            for celda in fila:
+                celda.border = borde
+                celda.alignment = Alignment(
+                    horizontal="center",
+                    vertical="center"
+                )
+        # Altura encabezado
+
+        ws.row_dimensions[6].height = 45
+        ws.row_dimensions[6].width = 55
+        
+        # Estado
+
+        for fila in range(7, ws.max_row+1):
+
+            estado = ws[f"E{fila}"]
+
+            if estado.value:
+
+                valor = str(estado.value).upper()
+
+
+                if valor in ["IRREGULAR","REGULAR"]:
+
+                    estado.fill = PatternFill(
+                        "solid",
+                        fgColor="FFF2CC"
+                    )
+
+
+                elif valor == "MALO":
+
+                    estado.fill = PatternFill(
+                        "solid",
+                        fgColor="F4CCCC"
+                    )
+
+
+                estado.alignment = Alignment(
+                    horizontal="center",
+                    vertical="center"
+                )
+
+
+        # Congelar encabezado
+
+        """ ws.freeze_panes="A7" """
+
+
+        # Filtro
+
+        ws.auto_filter.ref = f"A6:M6"
+
+    wb.save(archivo_excel)
+
+""" def insertar_imagenes(archivo_excel, resultado_formulario, carpeta_fotos):
+    wb = load_workbook(archivo_excel)
+
+    barrios= defaultdict(list)
+
+    for registro in resultado_formulario:
+        barrios[normalizar_barrio(registro["BARRIO"])].append(registro)
+
+    for barrio, registros in barrios.items():
+        ws = wb[str(barrio)[:31]]
+
+        col_antes = ws.max_column - 1
+        col_despues = ws.max_column 
+
+        for fila, registro in enumerate(registros,start=7):
+
+            barrio = registro["BARRIO"]
+
+            id_elemento = registro["ID_ELEMENTO"]
+
+            if  id_elemento == "" or id_elemento is None:
+                id_elemento = registro["DIRECCIÓN"]
+
+            ruta_antes = os.path.join(
+                carpeta_fotos,
+                barrio,
+                f"{id_elemento}_antes.jpg"
+            )
+
+            ruta_despues = os.path.join(
+                carpeta_fotos,
+                barrio,
+                f"{id_elemento}_despues.jpg"
+            )
+
+            #ws.row_dimensions[fila].height = 90
+
+            if os.path.exists(ruta_antes):
+
+                img = Image(ruta_antes)
+
+                img.width = 80
+                img.height = 150
+
+                ws.add_image(
+                    img,
+                    f"{get_column_letter(col_antes)}{fila}"
+                )
+                
+
+            if os.path.exists(ruta_despues):
+
+                img = Image(ruta_despues)
+
+                img.width = 80
+                img.height = 150
+
+                ws.add_image(
+                    img,
+                    f"{get_column_letter(col_despues)}{fila}"
+                )
+         # AJUSTAR ANCHOS
+        for col in ws.columns:
+
+            letra = get_column_letter(col[0].column)
+            if letra in [
+                get_column_letter(col_antes),
+                get_column_letter(col_despues)
+            ]:
+
+                ws.column_dimensions[letra].width = 20
+
+            else:
+
+                longitud = max(
+                    len(str(c.value)) if c.value else 0
+                    for c in col
+                )
+
+                ws.column_dimensions[letra].width = min(longitud+3,25)
+
+    wb.save(archivo_excel)
+ """
+
+
+
+
+
+
+
+def insertar_imagenes(archivo_excel, resultado_formulario, carpeta_fotos):
+
+    wb = load_workbook(archivo_excel)
+
+    barrios = agrupar_por_barrio(resultado_formulario)
+
+    for nombre_hoja, registros in barrios.items():
+
+        ws = wb[str(nombre_hoja)[:31]]
+
+        col_antes, col_despues = obtener_columnas_imagenes(ws)
+
+        for fila, registro in enumerate(registros, start=7):
+
+            insertar_imagen_fila(
+                ws,
+                fila,
+                registro,
+                carpeta_fotos,
+                col_antes,
+                col_despues
+            )
+
+        ajustar_anchos_columnas(ws, col_antes, col_despues)
+
+    wb.save(archivo_excel)
+
+
+def agrupar_por_barrio(resultado_formulario):
+
+    barrios = defaultdict(list)
+
+    for registro in resultado_formulario:
+        barrios[
+            normalizar_barrio(registro["BARRIO"])
+        ].append(registro)
+
+    return barrios
+
+def obtener_columnas_imagenes(ws):
+
+    col_antes = ws.max_column - 1
+    col_despues = ws.max_column
+
+    return col_antes, col_despues
+
+
+
+def obtener_rutas_imagenes(registro, carpeta_fotos):
+
+    barrio = registro["BARRIO"]
+
+    id_elemento = (
+        registro["ID_ELEMENTO"]
+        if registro["ID_ELEMENTO"]
+        else registro["DIRECCIÓN"]
+    )
+
+    ruta_antes = os.path.join(
+        carpeta_fotos,
+        barrio,
+        f"{id_elemento}_antes.jpg"
+    )
+
+    ruta_despues = os.path.join(
+        carpeta_fotos,
+        barrio,
+        f"{id_elemento}_despues.jpg"
+    )
+
+    return ruta_antes, ruta_despues
+
+def insertar_imagen_fila(
+    ws,
+    fila,
+    registro,
+    carpeta_fotos,
+    col_antes,
+    col_despues
+):
+
+    ruta_antes, ruta_despues = obtener_rutas_imagenes(
+        registro,
+        carpeta_fotos
+    )
+
+    if os.path.exists(ruta_antes):
+
+        insertar_imagen(
+            ws,
+            ruta_antes,
+            fila,
+            col_antes
+        )
+
+    if os.path.exists(ruta_despues):
+
+        insertar_imagen(
+            ws,
+            ruta_despues,
+            fila,
+            col_despues
+        )
+
+def insertar_imagen(ws, ruta, fila, columna):
+
+    img = Image(ruta)
+
+    img.width = 80
+    img.height = 140
+
+    centrar_imagen(ws, img, fila, columna)
+
+    ws.add_image(img)
+
+
+def centrar_imagen(ws, img, fila, columna):
+
+    ancho_columna_px = 140
+    alto_fila_px = 120
+
+    offset_x = max((ancho_columna_px - img.width) // 2, 0)
+    offset_y = max((alto_fila_px - img.height) // 2, 0)
+
+    marker = AnchorMarker(
+        col=columna - 1,
+        row=fila - 1,
+        colOff=pixels_to_EMU(offset_x),
+        rowOff=pixels_to_EMU(offset_y)
+    )
+
+    img.anchor = OneCellAnchor(
+    _from=marker,
+    ext=XDRPositiveSize2D(
+        pixels_to_EMU(img.width),
+        pixels_to_EMU(img.height)
+    )
+)
+
+def ajustar_anchos_columnas(
+    ws,
+    col_antes,
+    col_despues
+):
+
+    for col in ws.columns:
+
+        letra = get_column_letter(col[0].column)
+
+        if letra in (
+            get_column_letter(col_antes),
+            get_column_letter(col_despues)
+        ):
+
+            ws.column_dimensions[letra].width = 20
+
+        else:
+
+            longitud = max(
+                len(str(c.value)) if c.value else 0
+                for c in col
+            )
+
+            ws.column_dimensions[letra].width = min(
+                longitud + 3,
+                25
+            )
+
+
+def proceso():
+    session= crear_sesion()
+    uid = obtener_uid(session)
+
+    datos = obtener_formulario(session, uid)
+
+    resultado = procesar_datos(datos)
+
+    crear_excel(resultado, session)
+
+
+
+""" def proceso():
+    try:
+        session = crear_sesion()
+        uid = obtener_uid(session)
+
+        datos = obtener_formulario(session, uid)
+
+        resultado = procesar_datos(datos)
+
+        crear_excel(resultado, session)
+
+        return True
+
+    except Exception as e:
+        print(f"Error en proceso: {e}")
+        return False """
+
---
+++ main.py
@@ -0,0 +1,170 @@
+import tkinter as tk
+from tkinter import ttk, filedialog, Image
+from PIL import Image, ImageTk
+import threading
+import time
+
+from kobo_api import proceso,obtener_uid, crear_sesion, obtener_formulario, procesar_datos, descargar_fotos, crear_excel, insertar_imagenes 
+
+""" def iniciar_descarga():
+
+    hilo = threading.Thread(target=proceso)
+    hilo.start() """
+
+#def actualizarDatos():
+
+
+
+
+
+def centrar_ventana(ventana, ancho, alto):
+    # 1. Obtener las dimensiones de la pantalla
+    pantalla_ancho = ventana.winfo_screenwidth()
+    pantalla_alto = ventana.winfo_screenheight()
+    
+    # 2. Calcular las coordenadas X e Y para centrar
+    x = int((pantalla_ancho - ancho) / 2)
+    y = int((pantalla_alto - alto) / 2)
+    
+    # 3. Aplicar la geometría a la ventana
+    ventana.geometry(f"{ancho}x{alto}+{x}+{y}")
+# -------------------------
+# Ventana
+# -------------------------
+
+ventana = tk.Tk()
+
+ventana.title("Descarga de Imagenes y Creación de Excel")
+
+centrar_ventana(ventana, 600, 400) 
+
+imagen = Image.open("logo.jpeg").convert("RGBA")
+
+# Ajustarla al tamaño de la ventana
+imagen = imagen.resize((600, 400), Image.Resampling.LANCZOS)
+imagen.putalpha(60)
+
+# Convertirla al formato de Tkinter
+fondo = ImageTk.PhotoImage(imagen)
+# Colocarla como fondo
+canvas = tk.Canvas(
+    ventana,
+    width=600,
+    height=400,
+    highlightthickness=0,
+    bd=0
+)
+canvas.pack(fill="both", expand=True)
+
+canvas.create_image(0, 0, image=fondo, anchor="nw")
+
+canvas.create_text(
+    300,
+    40,
+    text="Mantenimiento de sumideros",
+    fill="darkblue",
+    font=("Arial", 16, "bold")
+)
+
+canvas.create_text(
+    150,
+    80,
+    text="Actualizar carpeta existente",
+    fill="blue",
+    font=("Arial", 14)
+)
+
+canvas.create_text(
+    270,
+    110,
+    text="Selecciona una carpeta ya creada que desee actualizar (solo la carpeta del semestre)",
+    fill="navy",
+    font=("Arial", 10, )
+)
+
+def direccionArchivoExcel():
+    carpeta_destino = filedialog.askdirectory(title="Seleccionar Carpeta para Guardar el Excel")
+    if carpeta_destino:
+        print(f"Carpeta seleccionada: {carpeta_destino}")
+        return carpeta_destino
+    else:
+        return ""
+    
+def actualizar_archivo():
+    carpeta = direccionArchivoExcel()
+    if(carpeta):
+        print(carpeta)
+    else:
+        print("eror al conseguir direccion")
+
+boton = tk.Button(
+    ventana,
+    text="Aceptar",
+    command=actualizar_archivo
+)
+
+canvas.create_window(
+    300,          # x
+    150,          # y
+    window=boton
+)
+
+
+def iniciar_proceso(ventana=ventana):
+    # Crear ventana de progreso
+    ventanaBarra = tk.Toplevel(ventana)
+    ventanaBarra.title("Procesando")
+    centrar_ventana(ventanaBarra, 350, 100)
+    ventanaBarra.resizable(False, False)
+
+    lbl_estado = ttk.Label(
+        ventanaBarra,
+        text="Generando el archivo de Excel...\n"
+             "Este proceso puede tardar unos minutos, por favor espere."
+    )   
+    lbl_estado.pack(pady=10)
+
+    barra = ttk.Progressbar(
+        ventanaBarra,
+        mode="indeterminate",
+        length=300
+    )
+    barra.pack(pady=10)
+    barra.start(10)
+
+    def tarea():
+        try:
+            proceso()
+        finally:
+            # Volver al hilo principal para cerrar la ventanaBarra
+            ventanaBarra.after(0, finalizar)
+
+    def finalizar():
+        barra.stop()
+        barra.pack_forget()
+        lbl_estado.config(
+            text="✅ Proceso finalizado correctamente."
+        )
+    
+    ttk.Button(
+            ventanaBarra,
+            text="Cerrar",
+            command=ventanaBarra.destroy
+        ).pack(pady=10)
+
+    threading.Thread(target=tarea, daemon=True).start()
+
+
+btn_derecho = tk.Button(
+    ventana,
+    text="Crear Excel",
+    width=15,
+    command=iniciar_proceso
+    #command=direccionArchivoExcel
+    
+)
+btn_derecho.pack(side="bottom", padx=20, pady=20) 
+
+ventana.resizable(False, False)
+ventana.mainloop()
+
